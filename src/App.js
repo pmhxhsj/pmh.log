2dcd4486ded0aa8d97f7de9665a9ab8a5294c9ad
pmhxhsj <minho7952@naver.com>
2022-04-03 20:28:51 +0900

    feat: dash board page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import styles from './style/index.module.css';
+import DashImage from './SteadyLife_Dash.png';
 
 function App() {
-  return <div className="App">Blog!</div>;
+  return (
+    <div className="App">
+      <Header />
+      <div className={styles.Margin}>
+        <h1>🏠 Dash Board</h1>
+        <br />
+        <img src={DashImage} alt="dashImage" width="800px" />
+        <div className={styles.MainDiv}>
+          <div>das</div>
+          <div>das</div>
+          <div>das</div>
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
