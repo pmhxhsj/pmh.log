77508c42c4bf1f4ac6685b11baeac03e67ddb5ba
pmhxhsj <minho7952@naver.com>
2022-04-04 00:39:17 +0900

    feat: footer edit

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,19 +1,10 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react';
 import styles from '../style/index.module.css';
-// eslint-disable-next-line react/react-in-jsx-scope
-// eslint-disable-next-line arrow-body-style
+
 const myFooter = () => {
-  function openLink() {
-    alert('pmhxxhsj@gmail.com');
-  }
   return (
     <footer className={styles.Footer}>
       <p>
-        <a href="#" onClick={openLink}>
-          Email
-        </a>
-        &nbsp;|&nbsp;
         <a href="https://parkparkpark.tistory.com">Blog</a>
         &nbsp;|&nbsp;
         <a href="https://github.com/pmhxhsj">Github</a>
